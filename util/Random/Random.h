c696f8932641b9374a4ba1f04d52321cad2cfdf8
debyecao <debyecao@tencent.com>
2020-11-24 15:19:11 +0800

    add random util

---
+++ util/Random/Random.h
@@ -0,0 +1,26 @@
+//
+// Created by debyecao on 11/24/20.
+//
+
+#ifndef DRENDER_RANDOM_H
+#define DRENDER_RANDOM_H
+
+class Random final
+{
+public:
+	void InitializeSeedRandom();
+	void InitializeSeed(unsigned int seed);
+
+	int IntRange(int min, int max);
+	float FloatRange(float min, float max);
+	double DoubleRange(double min, double max);
+
+private:
+	Random() = delete;
+	~Random() = delete;
+
+	Random(const Random&) = delete;
+	Random& operator=(const Random&) = delete;
+};
+
+#endif//DRENDER_RANDOM_H
